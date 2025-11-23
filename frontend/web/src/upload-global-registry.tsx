63abe88088e60a7d56669811a92c3d55914eb043
beikshikapam <beikshikapam@users.noreply.github.com>
2025-11-23 12:02:12 +0000

    Add upload-global-registry.tsx, validator.js, vercel.json

---
+++ frontend/web/src/upload-global-registry.tsx
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+interface common_orderProps { data: string }
+
---
+++ frontend/web/src/validator.js
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+export default function interface_fragments() { return <div>Component</div>; }
+
